51f071a1b96281549c69409d28a901939c22f4d3
Jimmy Kaiser <jimmykaiser@gmail.com>
2020-10-02 00:12:24 -0400

    script to update data sets

--- pull-data.sh
+++ pull-data.sh
@@ -1,6 +1,14 @@
 # Update repo
 git fetch upstream
 git rebase -X theirs upstream/master
-# Get data from seven days prior
+# Go to latest commit in NYC Health repo
 git checkout upstream/master
-git cat-file -p HEAD~7:data-by-modzcta.csv > data-by-modzcta-last-week.csv
+# Assuming one commit per day...
+# Get data from seven days prior
+SEVEN_DAYS=$(git log --format=%cr -n 1 HEAD~7 | tr ' ' '-')
+git cat-file -p HEAD~7:data-by-modzcta.csv > "data-by-modzcta-$SEVEN_DAYS.csv"
+# Get data from two weeks prior
+TWO_WEEKS=$(git log --format=%cr -n 1 HEAD~14 | tr ' ' '-')
+git cat-file -p HEAD~7:data-by-modzcta.csv > "data-by-modzcta-$TWO_WEEKS.csv"
+# Go back to forked repo
+git checkout master
