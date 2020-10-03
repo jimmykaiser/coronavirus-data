a5710020bd392b8e2111c38de258fb0f3c77ed1e
Jimmy Kaiser <jimmykaiser@gmail.com>
2020-10-02 23:17:44 -0400

    adjustments to script:

--- pull-data.sh
+++ pull-data.sh
@@ -8,10 +8,10 @@ today_hash=$(git log --after="`./get_date.py 1`" --before="`./get_date.py 0`" --
 week_ago_hash=$(git log --after="`./get_date.py 8`" --before="`./get_date.py 7`" --pretty=format:"%h" -n 1)
 two_week_hash=$(git log --after="`./get_date.py 15`" --before="`./get_date.py 14`" --pretty=format:"%h" -n 1)
 # Get data from seven days prior
-SEVEN_DAYS=$(git log --format=%ci -n 1 HEAD~7 | tr ' ' '-')
+SEVEN_DAYS=$(git log --format=%ad --date=short -n 1 $week_ago_hash | tr ' ' '-')
 git cat-file -p $week_ago_hash:data-by-modzcta.csv > "data-by-modzcta-$SEVEN_DAYS.csv"
 # Get data from two weeks prior
-TWO_WEEKS=$(git log --format=%ci -n 1 HEAD~14 | tr ' ' '-')
+TWO_WEEKS=$(git log --format=%ad --date=short -n 1 $two_week_hash | tr ' ' '-')
 git cat-file -p $two_week_hash:data-by-modzcta.csv > "data-by-modzcta-$TWO_WEEKS.csv"
 # Go back to forked repo
 git checkout master
