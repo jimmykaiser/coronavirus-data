29a72ac5cf3d283c4e9142fe510214cc49882263
Jimmy Kaiser <jimmykaiser@gmail.com>
2020-11-19 21:37:30 -0500

    align to new data source

--- make_map.py
+++ make_map.py
@@ -36,51 +36,42 @@ def get_citywide_data(citywide_file, days_ago):
 
 def import_file(date_wanted):
     """ 
-    Import neighborbood data from latest commit,
-    one week before latest commit, and two weeks before latest commit 
+    Import neighborbood data
     """
-    df = pd.read_csv(f"data-by-modzcta-{date_wanted}.csv")
+    df = pd.read_csv(f"last7days-by-modzcta-{date_wanted}.csv")
     df.columns = [c.lower() for c in df.columns]
+    df.columns = ["modified_zcta","neighborhood_name","percent_positive","total_covid_tests","covid_case_count",
+    "median_daily_test_rate","adequately_tested","date_range"]
     return df
 
-def merge_data(this_week, last_week, two_weeks_ago):
+def merge_data(this_week, last_week):
     """ 
     Merge neighborhood case counts and covid tests from one week ago
-    and two weeks ago onto latest data available
+    onto latest data available
     """
     # Check positivity rate calculation
-    this_week["percent_check"] = ((this_week["covid_case_count"] / this_week["total_covid_tests"])* 100).round(2)
-    # Remove 11096 which is missing name, population, and rates
-    this_week = this_week[this_week.modified_zcta != 11096]
-    # assert len(this_week[this_week.positive_check != this_week.percent_positive]) == 0
+    this_week["positive_check"] = ((this_week["covid_case_count"] / this_week["total_covid_tests"])* 100).round(2)
+    assert len(this_week[this_week.positive_check != this_week.percent_positive]) == 0
     # Merge data sets
-    id_cols = ["modified_zcta", "neighborhood_name", "borough_group"]
-    cols_wanted = id_cols+["covid_case_count", "total_covid_tests"]
-    df = this_week[cols_wanted+["pop_denominator"]].merge(last_week[cols_wanted], how="left", on=id_cols, suffixes=["", "_last_week"])
-    df = df.merge(two_weeks_ago[cols_wanted], how="left", on=id_cols, suffixes=["", "_two_weeks_ago"])
+    id_cols = ["modified_zcta", "neighborhood_name"]
+    cols_wanted = id_cols+["covid_case_count", "total_covid_tests", "date_range"]
+    df = this_week[cols_wanted].merge(last_week[cols_wanted], how="left", on=id_cols, suffixes=["", "_last_week"])
     return df
 
 def prep_stats(df):
     """
-    Calculate cases and tests in last 7 days, positivity rate over 
-    last 7 days, and average daily cases per 100,000 people over 7 days.
+    Calculate cases and tests in last 7 days and positivity rate over 
+    last 7 days. 
     Calculate the same for cases and tests in the week before last 7 days.
     """
     df = df.copy(deep=True)
     # %% Stats over the past week - today to 7 days ago
-    df["cases_past_week"] = df["covid_case_count"] - df["covid_case_count_last_week"]
-    # some places where total cases were higher past week than this week...
-    df["cases_past_week"] = df["cases_past_week"].clip(0, None)
-    df["tests_past_week"] = df["total_covid_tests"] - df["total_covid_tests_last_week"]
+    df["cases_past_week"] = df["covid_case_count"]
+    df["tests_past_week"] = df["total_covid_tests"]
     df["positivity_rate_past_week"] = ((df["cases_past_week"] / df["tests_past_week"]) * 100).round(1)
-    df["case_rate_past_week"] = (((df["cases_past_week"] / df["pop_denominator"]) * 100000) / 7).round()
 
     # %% Stats over week before last - 7 days ago to 14 days ago
-    df["cases_week_before_last"] = df["covid_case_count_last_week"] - df["covid_case_count_two_weeks_ago"]
-    df["cases_week_before_last"] = df["cases_week_before_last"].clip(0, None)
-    df["tests_week_before_last"] = df["total_covid_tests_last_week"] - df["total_covid_tests_two_weeks_ago"]
-    df["positivity_rate_week_before_last"] = ((df["cases_week_before_last"] / df["tests_week_before_last"]) * 100).round(1)
-    df["case_rate_week_before_last"] = (((df["cases_week_before_last"] / df["pop_denominator"]) * 100000) / 7).round()
+    df["positivity_rate_week_before_last"] = ((df["covid_case_count_last_week"] / df["total_covid_tests_last_week"]) * 100).round(1)
 
     return df
 
@@ -93,7 +84,7 @@ def produce_map(df, nycmap, map_name):
     # Formatting
     df["Positivity Rate (%)"] = df["positivity_rate_past_week"]
     df["neighborhood"] = df["neighborhood_name"].str.replace("/"," /<br>")
-    df["population"] = df["pop_denominator"].round().astype(int).apply(lambda x : "{:,}".format(x))
+    # df["population"] = df["pop_denominator"].round().astype(int).apply(lambda x : "{:,}".format(x))
     df["tests_past_week"] = df["tests_past_week"].round().astype(int).apply(lambda x : "{:,}".format(x))
 
     # Make map
@@ -103,7 +94,7 @@ def produce_map(df, nycmap, map_name):
         locations="modified_zcta",
         featureidkey="properties.MODZCTA",
         color="Positivity Rate (%)",
-        range_color=[0, 15],
+        range_color=[0, 10],
         color_continuous_scale="Portland",
         mapbox_style="carto-positron",
         zoom=9, 
@@ -111,12 +102,13 @@ def produce_map(df, nycmap, map_name):
         opacity=0.7,
         custom_data=[
                 "neighborhood", 
-                "population", 
+                "date_range",
+                # "population", 
                 "cases_past_week", 
                 "tests_past_week",
                 "positivity_rate_week_before_last", 
-                "case_rate_past_week",
-                "case_rate_week_before_last",  
+                # "case_rate_past_week",
+                # "case_rate_week_before_last",  
         ],
         width=600, height=500
     )
@@ -125,37 +117,34 @@ def produce_map(df, nycmap, map_name):
         hovertemplate="<br>".join([
             "<b>%{customdata[0]}</b>",
             "Zip code: %{location}",
-            "Population: %{customdata[1]}",
+            # "Population: %{customdata[1]}",
+            "Date Range: %{customdata[1]}", 
             "Cases in the past week: %{customdata[2]}",
             "Tests in the past week: %{customdata[3]}",
             "Postivity rate (%)<br>    This week: %{z}",
             "    Last week: %{customdata[4]}",
-            "Average daily cases per 100,000 people<br>    This week: %{customdata[5]}",
-            "    Last week: %{customdata[6]}",
+            # "Average daily cases per 100,000 people<br>    This week: %{customdata[5]}",
+            # "    Last week: %{customdata[6]}",
         ])
     )
     config = {'displaylogo': False}
     fig.write_html(map_name, config=config)
     return fig
 
-def update_md_file(citywide, citywide_last_week,latest_date, index_file):
+def update_md_file(citywide, citywide_last_week, index_file):
     """
     Update main page of website with new information.
     Write to file.
     """
 
-    latest_date_long = datetime.strptime(latest_date, '%Y-%m-%d').strftime('%B %-d, %Y')
+    # latest_date_long = datetime.strptime(latest_date, '%Y-%m-%d').strftime('%B %-d, %Y')
 
     md_str = f"""
 ## Positivity rate over the past week by neighborhood
 
 This map displays the percentage of Covid-19 tests that were positive over the last seven days for each New York City zip code. 
 
-While it is possible to view cumulative positivity rates by zip code since March on the NYC Dept. of Health's [data page](https://www1.nyc.gov/site/doh/covid/covid-19-data.page), this map shows changes in the past week so recent trends are evident. 
-
 {{% include_relative nyc-positivity.html%}}
-
-Map last updated {latest_date_long}
 
 ### Citywide numbers as of {citywide["date"]}
 
@@ -180,15 +169,15 @@ def make_new_map(latest_date):
     citywide_last_week = get_citywide_data(citywide_file, 7)
     this_week = import_file("today")
     last_week = import_file("last-week")
-    two_weeks_ago = import_file("two-weeks-ago")
-    df = merge_data(this_week, last_week, two_weeks_ago)
+    # two_weeks_ago = import_file("two-weeks-ago")
+    df = merge_data(this_week, last_week)
     df = prep_stats(df)
     fig = produce_map(df, nycmap, map_name)
-    md_str = update_md_file(citywide, citywide_last_week, latest_date, index_file)
+    md_str = update_md_file(citywide, citywide_last_week, index_file)
     print(md_str)
     return
 
 if __name__ == "__main__":
-    latest_date = sys.argv[1]
-    # latest_date = "2020-11-10"
+    # latest_date = sys.argv[1]
+    latest_date = "2020-11-19"
     make_new_map(latest_date)
--- update-data.sh
+++ update-data.sh
@@ -9,15 +9,15 @@ today_hash=$(git log upstream/master --pretty=format:"%h" -n 1)
 week_ago_hash=$(git log upstream/master --after="`./get_date.py $TODAY 8`" --before="`./get_date.py $TODAY 7`" --pretty=format:"%h" -n 1)
 two_week_hash=$(git log upstream/master --after="`./get_date.py $TODAY 15`" --before="`./get_date.py $TODAY 14`" --pretty=format:"%h" -n 1)
 # Get data from today
-git cat-file -p $today_hash:totals/data-by-modzcta.csv > "data-by-modzcta-today.csv"
+git cat-file -p $today_hash:latest/last7days-by-modzcta.csv > "last7days-by-modzcta-today.csv"
 echo "Got data as of $TODAY"
 # Get data from seven days prior
-git cat-file -p $week_ago_hash:data-by-modzcta.csv > "data-by-modzcta-last-week.csv"
+git cat-file -p $week_ago_hash:latest/last7days-by-modzcta.csv > "last7days-by-modzcta-last-week.csv"
 WEEK_AGO=$(git log $week_ago_hash --format=%ad --date=short -n 1 | tr ' ' '-')
 echo "Got data as of $WEEK_AGO"
 # Get data from two weeks prior
-git cat-file -p $two_week_hash:data-by-modzcta.csv > "data-by-modzcta-two-weeks-ago.csv"
-TWO_WEEKS=$(git log $two_week_hash --format=%ad --date=short -n 1 | tr ' ' '-')
-echo "Got data as of $TWO_WEEKS"
+# git cat-file -p $two_week_hash:data-by-modzcta.csv > "data-by-modzcta-two-weeks-ago.csv"
+# TWO_WEEKS=$(git log $two_week_hash --format=%ad --date=short -n 1 | tr ' ' '-')
+# echo "Got data as of $TWO_WEEKS"
 # Make new version of map
 python make_map.py $TODAY
